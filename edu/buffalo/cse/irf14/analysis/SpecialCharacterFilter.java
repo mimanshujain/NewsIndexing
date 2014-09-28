acb0b784e78cd095024bd8d6b71d02048a91bf55
Mimanshu Shisodia <mimanshu@buffalo.edu>
2014-09-27 20:09:05 -0700

    Special Character (Done by Jagvir Singh)

--- edu/buffalo/cse/irf14/analysis/SpecialCharacterFilter.java
+++ edu/buffalo/cse/irf14/analysis/SpecialCharacterFilter.java
@@ -11,10 +11,19 @@ public class SpecialCharacterFilter extends TokenFilter {
 	}
 
 	// Jagvir
-	private static final String splCharacterFilterGeneric = "[^(a-zA-Z0-9)\\w+(a-zA-Z0-9)\\-\\s@&.]";
-	private static final String splCharacter = "";
+//	private static final String splChrFilterMinusMathSymbol = "[a-zA-Z0-9\\^\\+\\*\\-]+";
+	private static final String splChrFilterMinusMathSymbol = "[a-zA-Z0-9]*[\\^\\+\\*\\-]+[a-zA-Z0-9]*";
+//	private static final String splChrMathOpr = "[@<|>\\/\\*\\+\\^\\=\\&[\\/\\_\\\\]+]+";
+	private static final String splChrMathOpr = "[@<|>\\/\\*\\+\\^=:;&_\\\\]";
+	private static final String splChrAlphaAlpha1 = 
+			"[(\\-)*([a-zA-Z\\+\\!\\#\\$\\%\\^\\&\\*\\()])+(\\-)*([a-zA-Z\\+\\!\\#\\$\\%\\^\\&\\*\\(\\)])+(\\-)*]+";
+	private static final String splChrAlphaAlpha = 
+			"([\\+\\!#$%\\^&\\*\\(\\)~])";
+	private static final String splChrAtRate="([a-zA-Z0-9]*)(@)([a-zA-Z0-9.]*)";
 	public Pattern checkSplCharacter = null;
 	public Matcher matchSplCharacter = null;
+	private String stringToSaveTemp = "";
+	private String stringToSaveTemp2 = "";
 
 	@Override
 	public boolean increment() throws TokenizerException {
@@ -22,19 +31,87 @@ public class SpecialCharacterFilter extends TokenFilter {
 
 		try {
 			if (tStream.hasNext()) {
+				
 				Token tk = tStream.next();
-				if (tk.getTermText() != null || tk.getTermText() != "") {
-					String temp = tk.getTermText();
 
+				if (tk.getTermText() != "" || tk.getTermText() != null) {
+					// Jagvir FInding all symbols save math operations
+					
+					String tempToken = tk.getTermText();
 					checkSplCharacter = Pattern
-							.compile(splCharacterFilterGeneric);
-					matchSplCharacter = checkSplCharacter.matcher(temp.trim());
+							.compile(splChrFilterMinusMathSymbol);
+					matchSplCharacter = checkSplCharacter.matcher(tempToken
+							.trim());
+					String temp = "";
+					int i = 0;
+					while (matchSplCharacter.find()) {
+						temp += matchSplCharacter.group();
+						
+					}
+					if (!temp.isEmpty())
+						tempToken = temp;
 
-					if (matchSplCharacter.find()) {
-						temp = matchSplCharacter.group();
+					// 21stSept math symbol code
+
+					if (!tempToken.trim().equals("") || tempToken != "") {
+						
+						stringToSaveTemp = tempToken.toString();
+						
+						stringToSaveTemp = stringToSaveTemp.replaceAll(
+								splChrMathOpr, "");
+						
+						stringToSaveTemp = stringToSaveTemp.replaceAll(
+								splChrAlphaAlpha, "");
+						
+						tempToken = stringToSaveTemp.trim();
+
+						// splCharacters
+						if (tempToken.matches(splChrAlphaAlpha)) {
+							
+							checkSplCharacter = Pattern
+									.compile(splChrAlphaAlpha);
+							matchSplCharacter = checkSplCharacter
+									.matcher(tempToken.trim());
+							if (matchSplCharacter.find()) {
+								stringToSaveTemp2 = tempToken.toString();
+
+								stringToSaveTemp = stringToSaveTemp.replaceAll(
+										"-", "");
+								tempToken = stringToSaveTemp.trim();
+								
+							}
+
+						}
+						if (tempToken.matches(splChrAlphaAlpha1)) {
+							
+							checkSplCharacter = Pattern
+									.compile(splChrAlphaAlpha1);
+							matchSplCharacter = checkSplCharacter
+									.matcher(tempToken.trim());
+							if (matchSplCharacter.find()) {
+								stringToSaveTemp2 = tempToken.toString();
+
+								stringToSaveTemp = stringToSaveTemp.replaceAll(
+										"-", "");
+								tempToken = stringToSaveTemp.trim();
+								
+							}
+
+						}
+//						if (matchSplCharacter.find()) {
+//							if (matchSplCharacter.group(2) != null)
+//								matchSplCharacter.replaceAll(matchSplCharacter.group(2));
+//							
+//							System.out.println();
+//						}
+						
+						
 
 					}
 
+					tk.setTermText(tempToken.trim());
+					// commit
+					return true;
 				}
 
 			}
@@ -49,7 +126,7 @@ public class SpecialCharacterFilter extends TokenFilter {
 	@Override
 	public TokenStream getStream() {
 		// TODO Auto-generated method stub
-		return null;
+		return tStream;
 	}
 
 }
